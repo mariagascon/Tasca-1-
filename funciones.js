7e44a29da08b9e37801b93a88071986e37d2459f
Your Name <you@example.com>
2018-01-19 14:41:00 +0100

    Commit 19/01/18

--- funciones.js
+++ funciones.js
@@ -1,6 +1,14 @@
-function calculohipoteca(capital, interes, plazo) {
+function calculohipoteca() {
+    var capital = document.getElementById("cap").value;
+    var interes = document.getElementById("int").value;
+    var plazo = document.getElementById("pla").value;
     var interesmensual = interes/12;
-    var cuota = capital*interesmensual/100*(1-(1+(interesmensual/100)-plazo));
+
+    var cuota = capital*interesmensual/(100*(1-Math.pow(1+interesmensual/100,-plazo)));
     console.log(cuota);
     return cuota;
+
+    document.getElementById("resultado").innerHTML = "La cuota mensual de tu hipoteca es: "+cuota+" euros";
 }
+
+
